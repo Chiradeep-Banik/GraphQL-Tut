af99d7b597ddd2a3e9e9140c8a9ab2fe7028ec74
chiradeep <chiradeep13@gmail.com>
2022-06-05 04:10:32 +0530

    Learning graphQL

---
+++ README.md
@@ -0,0 +1,4 @@
+# GRAPHQL
+
+- Single URL endpoint for all GraphQL queries
+    - The data that we return is based on the **query** itself
---
+++ src/index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { GraphQLSchema,GraphQLObjectType,GraphQLString,buildSchema } = require('graphql'); 
+
+const PORT = process.env.PORT || 1313;
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.json({
+        woking: true,
+        msg:"The server is LIVE"
+    });
+});
+
+// const dummyScema = new GraphQLSchema({
+//     query: new GraphQLObjectType({
+//         name: "dummy",
+//         fields: ()=>{
+//             return {
+//             dummyMsg: {
+//                 type : GraphQLString,
+//                 resolve: ()=>{return "Hello DUmmy !!"}
+//             }
+//         }}
+//     })
+// });
+
+// app.use('graphql',graphqlHTTP({
+//     graphiql: true,
+//     schema: dummyScema
+// }));
+var schema = buildSchema(`
+  type Query {
+    hello: String,
+    fuck: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+  fuck:()=>{
+    return "Fuck you";
+  }
+};
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.listen(PORT, () => {
+    console.log(`Listening at port ${PORT}`);
+});
