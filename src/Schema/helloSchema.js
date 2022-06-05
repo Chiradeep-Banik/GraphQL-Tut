618559eae54e5d11a3230276677458f67ff958d6
Banik <chiradee13@gmail.com>
2022-06-06 01:33:53 +0530

    Understanding structuring of query and resolver

--- src/Schema/helloSchema.js
+++ src/Schema/helloSchema.js
@@ -1,10 +1,23 @@
-const { buildSchema } = require('graphql');
+const { gql } = require('apollo-server');
 
-const schema = buildSchema(`
+const typedefs = gql(`
   type Query {
-    hello: String,
-    fuck: String
+    hello: String!,
+    fuck: String,
+    num: Int,
+    book: Book,
+    books:[Book],
+    author:Author,
   }
+  type Book {
+    title: String,
+    author: Author
+  }
+  type Author {
+    id: Int!,
+    name: String,
+  }
+
 `);
 
-module.exports = schema;
+module.exports = typedefs;
